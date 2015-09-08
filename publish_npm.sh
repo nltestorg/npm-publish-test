7a077342e9a4babc2835fab23b49bf675d5b30e6
John Huang <john.huang@CLEVER.COM>
2015-09-08 18:20:23 +0000

    Added logging to publish script. Change package name

--- publish_npm.sh
+++ publish_npm.sh
@@ -6,18 +6,18 @@ npm config set always-auth true
 
 cp package.json package.json.bkp
 branch=`git branch | grep "*"`
-#echo "branch is '${branch}'"
+echo "branch is '${branch}'"
 if [ "${branch}" != "* master" ]
 then
-  #echo "only works on master branch!"
+  echo "only works on master branch!"
   exit
 fi
 
-#echo "getting name"
+echo "getting name"
 name=`json -f package.json name`
-#echo "got ${name}, updating name"
+echo "got ${name}, updating name"
 json -I -f package.json -e "name='@clever/${name}'"
-#echo "updated name, updating registry"
+echo "updated name, updating registry"
 json -I -f package.json -e "publishConfig={registry:'https://registry.npmjs.org'}"
 npm publish
 mv package.json.bkp package.json
