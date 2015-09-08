329ba86c255687ff2aff123d5c445f4ae189834e
John Huang <john.huang@CLEVER.COM>
2015-09-08 18:02:28 +0000

    regular nodejitsu setup with npm publish script

--- publish_npm.sh
+++ publish_npm.sh
@@ -1,4 +1,18 @@
+npm cache clean
+echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" > ~/.npmrc
+npm config set registry https://registry.npmjs.org
+npm config set strict-ssl true
+npm config set always-auth true
+
 cp package.json package.json.bkp
+branch=`git branch | grep "*"`
+#echo "branch is '${branch}'"
+if [ "${branch}" != "* master" ]
+then
+  #echo "only works on master branch!"
+  exit
+fi
+
 #echo "getting name"
 NAME=`json -f package.json name`
 #echo "got ${NAME}, updating name"
