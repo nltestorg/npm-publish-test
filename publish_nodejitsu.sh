050dd2fc7a2719a289cf5e4992cef45a0a684b9f
John Huang <john.huang@CLEVER.COM>
2015-09-09 19:06:26 +0000

    Clean up echos

--- publish_nodejitsu.sh
+++ publish_nodejitsu.sh
@@ -2,7 +2,7 @@
 git rev-parse --verify master
 if [ $? != 0 ]
 then
-  echo "only works on master branch!"
+  echo "Will not publish to nodejitsu on non-master branch!"
   exit
 fi
 
@@ -20,18 +20,18 @@ npm config set //clever.registry.nodejitsu.com/:username "${npm_drone_username}"
 npm config set //clever.registry.nodejitsu.com/:email "${npm_drone_email}"
 npm config set //clever.registry.nodejitsu.com/:always-auth true
 
-echo "getting name"
+#echo "getting name"
 name=`node node_modules/json/lib/json.js -f package.json name`
-echo "got ${name}, updating name, publishing package"
+#echo "got ${name}, updating name, publishing package"
 if [[ $name == "@clever/"* ]]
 then
   package_name=${name#$"@clever/"}
-  echo "setting package name to ${package_name}"
+  #echo "setting package name to ${package_name}"
   node node_modules/json/lib/json.js -I -f package.json -e "this.name='${package_name}'"
 fi
-echo "setting registry"
+#echo "setting registry"
 node node_modules/json/lib/json.js -I -f package.json -e "this.publishConfig={registry:'https://clever.registry.nodejitsu.com'}"
-echo "publishing"
+#echo "publishing"
 npm publish
 
 publish_exit_status=$?
