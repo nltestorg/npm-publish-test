09308b2c4b86ab7bb548663108efc185553f88ea
John Huang <john.huang@CLEVER.COM>
2015-09-09 00:51:35 +0000

    Make npm drone publishing the default
    publish_npm script changed to publish_nodejitsu

---
+++ publish_nodejitsu.sh
@@ -0,0 +1,39 @@
+#set -eu
+
+cp package.json package.json.bkp
+cp ~/.npmrc ~/.npmrc.bkp
+
+npm cache clean
+npm config set registry https://clever.registry.nodejitsu.com/
+npm config set strict-ssl true
+npm config set always-auth true
+auth=`echo -n ${npm_drone_username}:${npm_drone_password} | base64`
+npm config set _auth $auth
+
+current_commit=`git log -1 HEAD --pretty=format:"%H"`
+master_commit=`git log -1 master --pretty=format:"%H"`
+git branch
+echo "current_commit is '${current_commit}'"
+echo "master_commit is '${master_commit}'"
+if [ "${current_commit}" != ${master_commit} ]
+then
+  echo "only works on master branch!"
+  exit
+fi
+
+npm install json
+echo "getting name"
+name=`node node_modules/json/lib/json.js -f package.json name`
+echo "got ${name}, updating name, publishing package"
+if [[ $name == "@clever/"* ]]
+then
+  package_name=${name#$"@clever/"}
+  echo "setting package name to ${package_name}"
+  node node_modules/json/lib/json.js -I -f package.json -e "this.name='${package_name}'"
+fi
+node node_modules/json/lib/json.js -I -f package.json -e "this.publishConfig={registry:'https://clever.registry.nodejitsu.com'}"
+npm publish
+
+npm uninstall json
+mv package.json.bkp package.json
+mv ~/.npmrc.bkp ~/.npmrc
